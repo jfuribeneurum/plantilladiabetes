199862827f0a8f4f5fc06395b31ce9c31d251f18
juan.uribe <desarrolladorfs1@neurum.com.co>
2024-07-08 11:18:21 -0500

    se agregan tablas actividad fisica y nutricion

--- css/assets/images/scripts/main.js
+++ css/assets/images/scripts/main.js
@@ -1,7 +1,49 @@
-// JavaScript para obtener la fecha actual y mostrarla en el formulario
+function toggleInsulinPumpFields() {
+    const insulinPumpSelect = document.getElementById('Bombainsulina');
+    const insulinPumpFields = document.getElementById('insulinPumpFields');
+    
+    if (insulinPumpSelect.value === 'yes') {
+        insulinPumpFields.style.display = 'block';
+    } else {
+        insulinPumpFields.style.display = 'none';
+    }
+}
+
+function toggleCateterObstructionFields() {
+    const cateterObstructionSelect = document.getElementById('obstruccionCateter');
+    const cateterObstructionFields = document.getElementById('cateterObstructionFields');
+    
+    if (cateterObstructionSelect.value === 'yes') {
+        cateterObstructionFields.style.display = 'block';
+    } else {
+        cateterObstructionFields.style.display = 'none';
+        document.getElementById('numObstrucciones').value = ''; // Limpiar el valor del campo de texto
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-    var systemDateSpan = document.getElementById('systemDate');
-    var currentDate = new Date();
-    var formattedDate = currentDate.getDate() + '/' + (currentDate.getMonth() + 1) + '/' + currentDate.getFullYear();
-    systemDateSpan.textContent = formattedDate;
+    // Mostrar tabla de agentes orales si cambia el valor
+    document.getElementById('agentesorales').addEventListener('change', function() {
+        var table = document.getElementById('tablaAgentesOrales');
+        table.style.display = this.value === 'siagentes' ? 'block' : 'none';
+    });
+
+    // Mostrar tabla de insulina si cambia el valor
+    document.getElementById('insulina').addEventListener('change', function() {
+        var table = document.getElementById('tablainsulina');
+        table.style.display = this.value === 'siinsulina' ? 'block' : 'none';
+    });
+
+    // Mostrar estrategia nutricional si cambia la selección de ajuste de insulina
+    document.getElementById('insulinaPrandial').addEventListener('change', function() {
+        var div = document.getElementById('estrategiaNutricional');
+        div.style.display = this.value === 'yes' ? 'block' : 'none';
+
+        var div2 = document.getElementById('insulinaPrandialTabla');
+        div2.style.display = this.value === 'yes' ? 'block' : 'none';
+    });
+
+    // Inicializar estado de visibilidad de campos según la carga inicial
+    toggleInsulinPumpFields();
+    toggleCateterObstructionFields();
 });
